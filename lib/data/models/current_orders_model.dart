1188b9625c17a67d635d9ca6e79cff07eb682396
unknown <Saeed@Qatan>
2025-03-04 19:48:47 +0300

    new update

--- lib/data/models/current_orders_model.dart
+++ lib/data/models/current_orders_model.dart
@@ -13,16 +13,6 @@ class CurrentOrdersModel {
     required this.imageUrl,
   });
 
-  static List<CurrentOrdersModel> getCurrentorder() {
-    List<CurrentOrdersModel> currentOrders = [
-  (CurrentOrdersModel(storeName: "Store 1", ownerName: "Owner 1", deliveryTime: 30.0, order_code: 1001, imageUrl: "url1")),
-  (CurrentOrdersModel(storeName: "Store 2", ownerName: "Owner 2", deliveryTime: 45.0, order_code: 1002, imageUrl: "url2")),
-  (CurrentOrdersModel(storeName: "Store 3", ownerName: "Owner 3", deliveryTime: 25.0, order_code: 1003, imageUrl: "url3")),
-  (CurrentOrdersModel(storeName: "Store 4", ownerName: "Owner 4", deliveryTime: 50.0, order_code: 1004, imageUrl: "url4")),
-    ];
-    
-    return currentOrders;
-  }
 
   factory CurrentOrdersModel.fromJson(Map<String, dynamic> json) {
     return CurrentOrdersModel(
